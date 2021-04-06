89c4b39d5b9aa543edd5d3fff76720bdcfe64a0f
Julian <juliantmayes@gmail.com>
2021-04-06 02:33:16 -0700

    made material button slider component and menu

--- stories/comps.stories.js
+++ stories/comps.stories.js
@@ -3,11 +3,15 @@ import React from 'react';
 //import components from comps//
 import { Button } from '../comps/Button';
 import { ButtonContainer } from '../comps/ButtonContainer';
-
+import { Menu } from '../comps/Menu';
+import { MaterialButtonSlider } from '../comps/MaterialButtonSlider';
 
 //export components to storybook//
 export const ButtonUI = Button;
-export const ButtonContainerUI = ButtonContainer
+export const ButtonContainerUI = ButtonContainer;
+export const MenuUI = Menu;
+export const MaterialButtonSliderUI =  MaterialButtonSlider;
+
 
 
 
