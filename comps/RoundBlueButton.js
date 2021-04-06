dc6fdd427fa5f6ebe54a7b4ebb70f15c6361cfea
Robert Smith <razzira11@gmail.com>
2021-04-06 03:12:55 -0700

    made branch and started creating components

---
+++ comps/RoundBlueButton.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { color } from '../public/color'
+
+const RoundBlueButtonUI = styled.button `
+
+height: 50px;
+width: 370px;
+border-radius: 10px;
+`
+export const RoundBlueButton = () => {
+
+    return (
+        <RoundBlueButton></RoundBlueButton>
+    )
+}
--- stories/comps.stories.js
+++ stories/comps.stories.js
@@ -3,12 +3,13 @@ import React from 'react';
 //import components from comps//
 import { Button } from '../comps/Button';
 import { ButtonContainer } from '../comps/ButtonContainer';
+import { RoundBlueButton } from '../comps/RoundBlueButton'
 
 
 //export components to storybook//
 export const ButtonUI = Button;
-export const ButtonContainerUI = ButtonContainer
-
+export const ButtonContainerUI = ButtonContainer;
+export const RoundBlueButtonUI = RoundBlueButton
 
 
 //dont touch//
