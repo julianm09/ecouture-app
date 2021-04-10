e313ae6bd6b729facfe6ee4f7fbd4ea26baa5f4a
Robert Smith <razzira11@gmail.com>
2021-04-09 17:39:02 -0700

    updated and finished button container

--- comps/RoundBlueButton.js
+++ comps/RoundBlueButton.js
@@ -7,10 +7,54 @@ const RoundBlueButtonUI = styled.button `
 height: 50px;
 width: 370px;
 border-radius: 10px;
+background-color: ${color.blue};
+margin-top: 20px;
+font-family: 'Nunito',sans-serif;
+font-size: x-large;
+color: white;
+border: 1px solid white;
 `
+
+const RoundButtonContainer = styled.div`
+
+display: flex;
+flex-direction: column;
+`
+
+const BottomText = styled.div`
+
+display: flex;
+color: white;
+font-family: 'Nunito',sans-serif;
+font-size: large;
+margin-top: 50px;
+max-width: 350px;
+`
+
+const TopText = styled.div`
+
+display: flex;
+color: white;
+font-family: 'Nunito',sans-serif;
+font-size: xx-large;
+max-width: 350px;
+justify-content: center;
+margin-bottom: 50px;
+`
+
+
 export const RoundBlueButton = () => {
 
     return (
-        <RoundBlueButton>test this button</RoundBlueButton>
+
+        <RoundButtonContainer>
+            <TopText>Where are you located?</TopText>
+        <RoundBlueButtonUI>Burnaby</RoundBlueButtonUI>
+        <RoundBlueButtonUI>Vancouver</RoundBlueButtonUI>
+        <RoundBlueButtonUI>North Vancouver</RoundBlueButtonUI>
+        <RoundBlueButtonUI>Richmond</RoundBlueButtonUI>
+        <BottomText>Shopping online contributes significantly more waste than in store.</BottomText>
+       </RoundButtonContainer>
+        
     )
 }
--- stories/comps.stories.js
+++ stories/comps.stories.js
@@ -3,37 +3,11 @@ import React from 'react';
 //import components from comps//
 import { Button } from '../comps/Button';
 import { ButtonContainer } from '../comps/ButtonContainer';
-import { RecycleButton } from '../comps/RecycleButton';
-import { StartButton } from '../comps/StartButton';
-import { AboutResourcesCont } from '../comps/AboutResourcesCont';
-
-
-//export components to storybook//
-export const ButtonUI = Button;
-export const ButtonContainerUI = ButtonContainer;
-export const RecycleButtonUI = RecycleButton;
-export const StartButtonUI = StartButton;
-export const AboutResourcesContUI = AboutResourcesCont;
-
-
-//dont touch//
-export default {
-  title: 'Components',
-};
-
-import React from 'react';
-
-//import components from comps//
-import { Button } from '../comps/Button';
-import { ButtonContainer } from '../comps/ButtonContainer';
 import { StoreInside} from '../comps/StoreInside';
 import { StoreContainer } from '../comps/StoreContainer';
 import { StoreText } from '../comps/StoreText';
 import { RoundBlueButton } from '../comps/RoundBlueButton'
-import { FiberButton } from '../comps/FiberButton';
-import { FiberButtonContainer } from '../comps/FiberButtonContainer';
-import { Menu } from '../comps/Menu';
-import { MaterialButtonSlider } from '../comps/MaterialButtonSlider';
+
 
 //export components to storybook//
 export const ButtonUI = Button;
@@ -41,16 +15,7 @@ export const ButtonContainerUI = ButtonContainer;
 export const StoreInsideUI = StoreInside;
 export const StoreContainerUI = StoreContainer;
 export const StoreTextUI = StoreText;
-export const RoundBlueButtonUI = RoundBlueButton
-export const ButtonContainerUI = ButtonContainer
-export const FiberButtonUI = FiberButton;
-export const FiberButtonContainerUI = FiberButtonContainer;
-
-//export components to storybook//
-export const ButtonUI = Button;
-export const ButtonContainerUI = ButtonContainer;
-export const MenuUI = Menu;
-export const MaterialButtonSliderUI =  MaterialButtonSlider;
+export const RoundBlueButtonUI = RoundBlueButton;
 
 //dont touch//
 export default {
